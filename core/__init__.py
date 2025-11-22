a13bb0896fa1f3e89f4764fbe09a782705ebaccf
AzizT-dev <aziz.explorer@gmail.com>
2025-11-22 13:41:25 +0000

    Add files via upload

---
+++ core/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+KAT Analysis - Core Package
+Core analysis and processing modules
+
+Author: Aziz T.
+Copyright: (C) 2025 KaT - All rights reserved
+License: GPLv3
+Version: 1.0.0
+"""
+
+__all__ = [
+    'analysis_engine',
+    'polygon_analysis',
+    'line_analysis',
+    'point_analysis',
+    'classification',
+    'layer_operations',
+    'results_handler',
+    'visualization',
+    'utils'
+]
