00765f8005724e6d27a323a695a941ecfbffb75e
AzizT-dev <aziz.explorer@gmail.com>
2025-11-22 13:43:05 +0000

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-KAT Overlap Plugin
+KAT Overlap Analysis Plugin
+Main plugin initialization
+
+Author: Aziz T.
+Copyright: (C) 2025 KaT - All rights reserved
+License: GPLv3
+Version: 1.0.0
 """
 
 def classFactory(iface):
+    """Load KATOverlap class from kat_overlap module"""
     from .kat_overlap import KATOverlap
     return KATOverlap(iface)
