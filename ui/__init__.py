c96e67e14821598495c371585507d0843d12ab1f
AzizT-dev <aziz.explorer@gmail.com>
2025-11-22 13:34:28 +0000

    Add files via upload

---
+++ ui/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+KAT Analysis - UI Package
+User interface modules
+
+Author: Aziz T.
+Copyright: (C) 2025 KaT - All rights reserved
+License: GPLv3
+Version: 1.0.0
+"""
+
+__all__ = ['kat_overlap_ui']
